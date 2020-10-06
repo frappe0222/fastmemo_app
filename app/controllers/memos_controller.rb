74014048580885d054c5b2cb2388cbb87f7bc59a
frappe <frappeccino0222@gmail.com>
2020-10-06 19:14:22 +0900

    エラーメッセージ表示　バリデーション

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,5 @@
 class MemosController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index  # indexアクション
     @memos = Memo.all.order("created_at DESC")
@@ -11,33 +12,49 @@ class MemosController < ApplicationController
   end
 
   def create #createアクション
-    Memo.create(memo_params)
-    redirect_to root_path
-
+    @memo = Memo.create(memo_params)
+    if @memo.save
+      flash[:notice] = "投稿できました"
+      redirect_to root_path
+    else
+      flash[:alert] = "文字を入力してください"
+      redirect_to new_memo_path
+    end
   end
 
   def show #showアクション
   end
 
   def edit
-    @memo = Memo.find(params[:id])
   end
 
   def update
-    @memo = Memo.find(params[:id])
-    @memo.update(memo_params)
+   if @memo.update(memo_params)
+    flash[:notice] = "投稿できました"
     redirect_to root_path
+   else
+    flash[:alert] = "文字を入力してください"
+    redirect_to edit_memo_path(@post.id)
+   end
   end
 
   def destroy
-    @memo = Memo.find(params[:id])
-    @memo.destroy
-    redirect_to root_path
+    if @memo.destroy
+      flash[:delete] = "削除しました"
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
    private
+
+    def set_post
+      @memo = Memo.find(params[:id])
+    end
+
     def memo_params
-    params.permit(:content)
+     params.permit(:content)
     end
   
 end
